b21c9979db276cdfec7a6e8dc60f8df808400379
Dov Grobgeld <dov.grobgeld@gmail.com>
2011-05-23 01:51:33 +0300

    Tried qobjectification which fails compilation.

--- QvivImageViewer.cc
+++ QvivImageViewer.cc
@@ -1,10 +1,21 @@
-//======================================================================
-//  QvivImageViewer.cc - 
-//
-//  Dov Grobgeld <dov.grobgeld@gmail.com>
-//  Tue May 10 12:27:30 2011
-//----------------------------------------------------------------------
-
+/*
+ * Copyright (C) 2011 Dov Grobgeld <dov.grobgeld@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 #include <QvivImageViewer.h>
 #include <QPen>
 #include <QPainter>
--- QvivImageViewer.h
+++ QvivImageViewer.h
@@ -1,10 +1,30 @@
+/*
+ * Copyright (C) 2011 Dov Grobgeld <dov.grobgeld@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 #ifndef QVIVIMAGEVIEWER_H
 #define QVIVIMAGEVIEWER_H
 
-#include <QWidget>
+#include <QImage>
 #include <QAbstractScrollArea>
 
 class QvivImageViewer : public QAbstractScrollArea {
+    Q_OBJECT 
+
 public:
     QvivImageViewer(QWidget *parent,
                     QImage image);
@@ -37,6 +57,13 @@ protected:
     void keyPressEvent (QKeyEvent * event);
     void scrollContentsBy (int dx, int dy);
     QSize sizeHint() const;
+
+signals:
+    // The image annotate callback is used for drawing overlays
+    // on the image.
+    void imageAnnotate(QImage image,
+                       int x0, int y0,
+                       double scale_x, double scale_y);
 
 private:
     class Priv;
